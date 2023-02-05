7b1dfdb096af1b2d28ede2830908d7762e08219d
Mathias Millet <mathias@mmill.eu>
2023-02-05 20:33:17 +0100

    remove unused scripts

--- scripts/package.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-mkdir -p extension_build
-pnpm build
-cd build
-zip -r -FS ../extension_build/extension.zip *
--- scripts/package_source.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-mkdir -p extension_build
-zip -r -FS ./extension_build/source.zip . --exclude '*.git*' --exclude 'node_modules*' --exclude 'build*' --exclude 'assets*'
